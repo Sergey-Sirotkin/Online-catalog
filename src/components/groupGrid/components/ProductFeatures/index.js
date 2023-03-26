e8e3583239bad6d10a164db2d0bc5b321d6aadf8
Sergey Sirotkin <sirotkin.san@gmail.com>
2023-03-26 21:50:50 +0300

    Code destructured

--- src/components/groupGrid/components/ProductFeatures/index.js
+++ src/components/groupGrid/components/ProductFeatures/index.js
@@ -2,41 +2,32 @@ import React from "react"
 import { Divider, Typography, Stack, Box, Link } from "@mui/material"
 import { AnchorLink } from "gatsby-plugin-anchor-links"
 
-const ProductFeatures = ({ features, handleOpacityChange,  }) => {
+const ProductFeatures = ({ features, handleOpacityChange }) => {
   const { color, type, group, availability, companion } = features[0]
 
   const { companionItem } = companion[0]
-
 
   const handleAnchorLinkClick = () => {
     handleOpacityChange(companionItem)
   }
 
+  const ordinaryItems = [
+    { label: "Колір:", value: color },
+    { label: "Тип малюнка:", value: type },
+    { label: "Група:", value: group },
+  ]
+
   return (
     <>
-      <Stack direction="row" gap={1} alignItems="center">
-        <Typography fontSize="18px" fontWeight="bold">
-          Колір:
-        </Typography>
+      {ordinaryItems.map(({ label, value }) => (
+        <Stack key={label} direction="row" gap={1} alignItems="center">
+          <Typography fontSize="18px" fontWeight="bold">
+            {label}
+          </Typography>
 
-        <Typography fontSize="18px">{color}</Typography>
-      </Stack>
-
-      <Stack direction="row" gap={1} alignItems="center">
-        <Typography fontSize="18px" fontWeight="bold">
-          Тип малюнка:
-        </Typography>
-
-        <Typography fontSize="18px">{type}</Typography>
-      </Stack>
-
-      <Stack direction="row" gap={1} alignItems="center">
-        <Typography fontSize="18px" fontWeight="bold">
-          Група:
-        </Typography>
-
-        <Typography fontSize="18px">{group}</Typography>
-      </Stack>
+          <Typography fontSize="18px">{value}</Typography>
+        </Stack>
+      ))}
 
       {companionItem ? (
         <Stack direction="row" gap={1} alignItems="center">
--- src/components/groupGrid/index.js
+++ src/components/groupGrid/index.js
@@ -1,39 +1,12 @@
 import React, { useCallback, useState } from "react"
 
-import {
-  Button,
-  Divider,
-  Card,
-  CardContent,
-  Grid,
-  Typography,
-  Stack,
-  Chip,
-  Dialog,
-} from "@mui/material"
+import { Stack, Chip } from "@mui/material"
 
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
-
-import ProductFeatures from "./components/ProductFeatures"
 import ProductDialog from "./components/ProductDialog"
+import ProductsGrid from "./components/ProductsGrid"
 
 const GroupGrid = ({ productCards, groupTitle, groupName }) => {
   const [lightboxImage, setLightboxImage] = useState(null)
-
-  const [opacity, setOpacity] = useState(1)
-  const [border, setBorder] = useState(null)
-  const [currentCompanion, setCurrentCompanion] = useState(null)
-
-  const handleOpacityChange = useCallback(companionItem => {
-    setOpacity(0.5)
-    setBorder("4px solid #1976d2")
-    setCurrentCompanion(companionItem)
-
-    setTimeout(() => {
-      setOpacity(1)
-      setBorder(null)
-    }, 2000)
-  }, [])
 
   const handleOpen = useCallback(({ productImage, itemNumber }) => {
     setLightboxImage({ productImage, itemNumber })
@@ -64,61 +37,7 @@ const GroupGrid = ({ productCards, groupTitle, groupName }) => {
         />
       </Stack>
 
-      <Grid container spacing={3}>
-        {productCards.map(({ image, itemNumber, features }) => {
-          const productImage = getImage(image)
-
-          return (
-            <Grid
-              id={`id${itemNumber}`}
-              key={itemNumber}
-              item
-              sm={12}
-              md={6}
-              lg={4}
-              xl={3}
-            >
-              <Card sx={{ height: "100%" }}>
-                <Stack
-                  alignItems="center"
-                  sx={{
-                    opacity: currentCompanion === itemNumber && opacity,
-                    border: currentCompanion === itemNumber && border,
-                    cursor: "pointer",
-                    transition: "all 0.3s",
-                    "&:hover": {
-                      opacity: 0.8,
-                    },
-                  }}
-                  onClick={() => handleOpen({ productImage, itemNumber })}
-                >
-                  <GatsbyImage image={productImage} alt={itemNumber} />
-                </Stack>
-
-                <CardContent
-                  sx={{
-                    p: { xs: 1, md: 2 },
-                    "&:last-child": {
-                      paddingBottom: 2,
-                    },
-                  }}
-                >
-                  <Typography fontWeight="bold" gutterBottom variant="h6">
-                    Артикул: {itemNumber}
-                  </Typography>
-
-                  <Divider sx={{ mb: 1 }} />
-
-                  <ProductFeatures
-                    features={features}
-                    handleOpacityChange={handleOpacityChange}
-                  />
-                </CardContent>
-              </Card>
-            </Grid>
-          )
-        })}
-      </Grid>
+      <ProductsGrid productCards={productCards} handleOpen={handleOpen} />
 
       {lightboxImage && (
         <ProductDialog
