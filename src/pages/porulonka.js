93526b586618d4f95757940b984d95a7000f04c3
Sergey Sirotkin <sirotkin.san@gmail.com>
2023-07-20 11:43:20 +0300

    20-07-23 upd

--- src/pages/porulonka.js
+++ src/pages/porulonka.js
@@ -1,48 +1,49 @@
-// import React from "react"
-// import { graphql } from "gatsby"
-// import GroupGrid from "../components/groupGridPorulonka"
+import React from "react"
+import { graphql } from "gatsby"
+import GroupGrid from "../components/groupGridPorulonka"
 
-// import Seo from "../components/seo"
-// import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Layout from "../components/layout"
 
-// const PorulonkaPage = ({ data }) => {
-//   const { groupName, productCards, groupTitle } = data.ecoline.frontmatter
+const PorulonkaPage = ({ data }) => {
+  const { groupName, productCards, groupTitle } = data.ecoline.frontmatter
 
-//   return (
-//     <Layout label="Порулонка - Слобожанские" isEcoLine="true">
-//       <GroupGrid
-//         productCards={productCards}
-//         groupTitle={groupTitle}
-//         groupName={groupName}
-//       />
-//     </Layout>
-//   )
-// }
+  return (
+    // <Layout label="Порулонка - Слобожанские" isEcoLine="true">
+    //   <GroupGrid
+    //     productCards={productCards}
+    //     groupTitle={groupTitle}
+    //     groupName={groupName}
+    //   />
+    // </Layout>
+    null
+  )
+}
 
-// export const Head = () => <Seo />
+export const Head = () => <Seo />
 
-// export const query = graphql`
-//   query {
-//     ecoline: mdx(frontmatter: { groupName: { eq: "porulonka" } }) {
-//       frontmatter {
-//         groupName
-//         groupTitle
-//         productCards {
-//           image {
-//             childImageSharp {
-//               gatsbyImageData
-//             }
-//           }
-//           itemNumber
-//           features {
-//             companion {
-//               companionItem
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query {
+    ecoline: mdx(frontmatter: { groupName: { eq: "porulonka" } }) {
+      frontmatter {
+        groupName
+        groupTitle
+        productCards {
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          itemNumber
+          features {
+            companion {
+              companionItem
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
-// export default PorulonkaPage
+export default PorulonkaPage
